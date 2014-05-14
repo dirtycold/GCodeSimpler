1b49e15ea69fe03fd9e57b53b0dbdc41fe4a5d1b
ZH <zhanghan@gmx.cn>
2014-05-14 14:31:34 +0800

    modified: update translation

--- GCodeSimpler.ts
+++ GCodeSimpler.ts
@@ -4,18 +4,42 @@
 <context>
     <name>GCodeSimpler</name>
     <message>
-        <location filename="gcodesimpler.cpp" line="86"/>
+        <location filename="gcodesimpler.cpp" line="78"/>
+        <source>Source file: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gcodesimpler.cpp" line="83"/>
+        <location filename="gcodesimpler.cpp" line="84"/>
         <source>Source file not accessible.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="gcodesimpler.cpp" line="90"/>
         <location filename="gcodesimpler.cpp" line="91"/>
+        <source>Not a GCode file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gcodesimpler.cpp" line="98"/>
+        <location filename="gcodesimpler.cpp" line="99"/>
         <source>Target folder not writable.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gcodesimpler.cpp" line="99"/>
+        <location filename="gcodesimpler.cpp" line="114"/>
+        <location filename="gcodesimpler.cpp" line="115"/>
         <source>Target file not writable.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gcodesimpler.cpp" line="119"/>
+        <source>Processing GCode...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gcodesimpler.cpp" line="183"/>
+        <source>GCode processing complete.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -42,53 +66,66 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="75"/>
+        <location filename="gcodesimplerwidget.cpp" line="76"/>
         <source>Destination: %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="76"/>
+        <location filename="gcodesimplerwidget.cpp" line="77"/>
         <source>GCode processing complete.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="76"/>
-        <source>The output was in the same folder</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="gcodesimplerwidget.cpp" line="89"/>
+        <location filename="gcodesimplerwidget.cpp" line="90"/>
         <source>Something wrong happens.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="99"/>
+        <location filename="gcodesimplerwidget.cpp" line="100"/>
         <source>Multiple files is not supported.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="101"/>
+        <location filename="gcodesimplerwidget.cpp" line="134"/>
+        <source>(C)2014 Shaanxi Hengtong </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gcodesimplerwidget.cpp" line="65"/>
+        <location filename="gcodesimplerwidget.cpp" line="102"/>
         <source>Filepath: %1.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="136"/>
-        <source>Not a GCode file.</source>
+        <location filename="gcodesimplerwidget.cpp" line="77"/>
+        <source>The output was in the same folder.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="141"/>
+        <location filename="gcodesimplerwidget.cpp" line="128"/>
         <source>File not accessable.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="147"/>
+        <location filename="gcodesimplerwidget.cpp" line="134"/>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
+</context>
+<context>
+    <name>QObject</name>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="147"/>
-        <source>Shaanxi Hengtong (C)2014</source>
+        <location filename="main.cpp" line="28"/>
+        <source>
+GCodeSimpler
+(C)2014 Shaanxi Hengtong
+=======================
+
+Usage: GcodeSimpler [OPTION] [FILE]
+Options:
+--help			-h		Display this help
+--convert &lt;filepath&gt;	-c &lt;filepath&gt;	Process GCode file
+</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- GCodeSimpler_zh_CN.ts
+++ GCodeSimpler_zh_CN.ts
@@ -4,19 +4,43 @@
 <context>
     <name>GCodeSimpler</name>
     <message>
-        <location filename="gcodesimpler.cpp" line="86"/>
+        <location filename="gcodesimpler.cpp" line="78"/>
+        <source>Source file: %1</source>
+        <translation>原文件：%1</translation>
+    </message>
+    <message>
+        <location filename="gcodesimpler.cpp" line="83"/>
+        <location filename="gcodesimpler.cpp" line="84"/>
         <source>Source file not accessible.</source>
         <translation>无法读取原始文件。</translation>
     </message>
     <message>
+        <location filename="gcodesimpler.cpp" line="90"/>
         <location filename="gcodesimpler.cpp" line="91"/>
+        <source>Not a GCode file.</source>
+        <translation>不是 GCode 文件。</translation>
+    </message>
+    <message>
+        <location filename="gcodesimpler.cpp" line="98"/>
+        <location filename="gcodesimpler.cpp" line="99"/>
         <source>Target folder not writable.</source>
         <translation>无法写入目标目录。</translation>
     </message>
     <message>
-        <location filename="gcodesimpler.cpp" line="99"/>
+        <location filename="gcodesimpler.cpp" line="114"/>
+        <location filename="gcodesimpler.cpp" line="115"/>
         <source>Target file not writable.</source>
         <translation>无法写入目标文件。</translation>
+    </message>
+    <message>
+        <location filename="gcodesimpler.cpp" line="119"/>
+        <source>Processing GCode...</source>
+        <translation>正在转换…</translation>
+    </message>
+    <message>
+        <location filename="gcodesimpler.cpp" line="183"/>
+        <source>GCode processing complete.</source>
+        <translation>GCode 转换完毕。</translation>
     </message>
 </context>
 <context>
@@ -42,54 +66,88 @@
         <translation>正在转换…</translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="75"/>
+        <location filename="gcodesimplerwidget.cpp" line="76"/>
         <source>Destination: %1</source>
         <translation>输出文件路径：%1</translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="76"/>
+        <location filename="gcodesimplerwidget.cpp" line="77"/>
         <source>GCode processing complete.</source>
         <translation>GCode 转换完毕。</translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="76"/>
-        <source>The output was in the same folder</source>
-        <translation>输出文件在相同目录下</translation>
+        <location filename="gcodesimplerwidget.cpp" line="77"/>
+        <source>The output was in the same folder.</source>
+        <translation>输出文件在相同目录下。</translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="89"/>
+        <source>The output was in the same folder</source>
+        <translation type="obsolete">输出文件在相同目录下</translation>
+    </message>
+    <message>
+        <location filename="gcodesimplerwidget.cpp" line="90"/>
         <source>Something wrong happens.</source>
         <translation>出现错误。</translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="99"/>
+        <location filename="gcodesimplerwidget.cpp" line="100"/>
         <source>Multiple files is not supported.</source>
         <translation>不支持同时拖放多个文件。</translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="101"/>
+        <location filename="gcodesimplerwidget.cpp" line="134"/>
+        <source>(C)2014 Shaanxi Hengtong </source>
+        <translation>2014 陕西恒通智能机器有限公司 版权所有</translation>
+    </message>
+    <message>
+        <location filename="gcodesimplerwidget.cpp" line="65"/>
+        <location filename="gcodesimplerwidget.cpp" line="102"/>
         <source>Filepath: %1.</source>
         <translation>原文件路径：%1.</translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="136"/>
         <source>Not a GCode file.</source>
-        <translation>不是 GCode 文件。</translation>
+        <translation type="obsolete">不是 GCode 文件。</translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="141"/>
+        <location filename="gcodesimplerwidget.cpp" line="128"/>
         <source>File not accessable.</source>
         <translation>无法访问文件。</translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="147"/>
+        <location filename="gcodesimplerwidget.cpp" line="134"/>
         <source>About</source>
         <translation>关于</translation>
     </message>
     <message>
-        <location filename="gcodesimplerwidget.cpp" line="147"/>
         <source>Shaanxi Hengtong (C)2014</source>
-        <translation>2014 陕西恒通智能机器有限公司 版权所有</translation>
+        <translation type="obsolete">2014 陕西恒通智能机器有限公司 版权所有</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="28"/>
+        <source>
+GCodeSimpler
+(C)2014 Shaanxi Hengtong
+=======================
+
+Usage: GcodeSimpler [OPTION] [FILE]
+Options:
+--help			-h		Display this help
+--convert &lt;filepath&gt;	-c &lt;filepath&gt;	Process GCode file
+</source>
+        <translation>
+GCode转换器
+2014 陕西恒通智能机器有限公司 版权所有
+=======================
+
+用法: GcodeSimpler [选项] [文件名]
+选项:
+--help			-h		显示帮助信息
+--convert &lt;filepath&gt;	-c &lt;filepath&gt;	转换GCode文件
+</translation>
     </message>
 </context>
 </TS>
